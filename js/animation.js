f850f450d1d25c5bcfe4352d6d5484f14fa9af87
harsimrat <harsimrat11@gmail.com>
2023-06-20 10:25:42 +0530

    canvas ball moving

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,33 @@
 let canvas = document.getElementById("my_canvas");
 let context = canvas.getContext("2d");
 
-context.moveTo(100, 100);
-context.lineTo(100, 500);
-context.stroke();
+const width = canvas.width;
+const height = canvas.height;
+const speed = 4;
+let moveSpeed = speed;
+let radius = 70;
+let position = 50;
+
+function moveCircle(){
+    if(position+radius >= width){
+        moveSpeed = -speed;
+    }else if(position-radius <=0){
+        moveSpeed = speed;
+    }
+    position = position + moveSpeed;
+}
+
+function drawCircle(){
+    context.clearRect(0,0, width, height);
+    context.fillStyle = '#00ff00';
+    context.beginPath();
+    context.arc(position, 100, radius, 0, 2 * Math.PI, false);
+    context.fill();
+}
+
+function animate(){
+    moveCircle();
+    drawCircle();
+    window.requestAnimationFrame(animate);
+}
+window.requestAnimationFrame(animate);
